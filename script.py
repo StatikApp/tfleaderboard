aecde972b3703668f452194cfb7940c1ad1a3d54
Soracle <anemyfeed@gmail.com>
2024-01-24 05:36:06 +0100

    Change leaderboard

--- script.py
+++ script.py
@@ -5,5 +5,18 @@ response = requests.get('https://storage.googleapis.com/embark-discovery-leaderb
 
 data = response.json()
 
+for entry in data:
+    entry['rank'] = entry.pop('r', None)
+    entry['fame'] = entry.pop('f', None)
+    entry['old_fame'] = entry.pop('of', None)
+    entry['old_rank'] = entry.pop('or', None)
+    entry['cashouts'] = entry.pop('c', None)
+
+    entry['networks'] = {
+        'steam': entry.pop('steam', None),
+        'psn': entry.pop('psn', None),
+        'xbox': entry.pop('xbox', None)
+    }
+
 with open('leaderboard.json', 'w') as json_file:
     json_file.write(json.dumps(data, indent=2))
