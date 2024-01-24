5a932b9c6b3a23b5a83ff45499d1e13904030aa9
Soracle <anemyfeed@gmail.com>
2024-01-24 05:22:26 +0100

    workflow

--- script.py
+++ script.py
@@ -1,22 +1,9 @@
-from flask import Flask, jsonify
+import json
 import requests
 
-app = Flask(__name__)
+response = requests.get('https://storage.googleapis.com/embark-discovery-leaderboard/leaderboard-crossplay-discovery-live.json')
 
-@app.route('/leaderboard', methods=['GET'])
-def get_leaderboard():
-    # Effectuer une requête HTTP vers l'API de The Finals pour obtenir le leaderboard
-    # Remplacez l'URL et les paramètres en fonction de l'API que vous utilisez
-    response = requests.get('https://storage.googleapis.com/embark-discovery-leaderboard/leaderboard-crossplay-discovery-live.json')
-    
-    # Traiter la réponse JSON
-    data = response.json()
-    
-    # Stocker les données dans un fichier JSON
-    with open('leaderboard.json', 'w') as json_file:
-        json_file.write(json.dumps(data, indent=2))
-    
-    return jsonify(data)
+data = response.json()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+with open('leaderboard.json', 'w') as json_file:
+    json_file.write(json.dumps(data, indent=2))
